38d1855b3423bf0c1715601a73f52a3d65b181aa
semio <semio@posteo.net>
2024-07-13 12:05:26 +0800

    add demographic indicators

--- etl/scripts/demographic_indicators.py
+++ etl/scripts/demographic_indicators.py
@@ -5,15 +5,34 @@ import pandas as pd
 from ddf_utils.str import to_concept_id
 
 # %%
-source_file =  "../source/WPP2022_Demographic_Indicators_Medium.zip"
+# as of 7/13/2024, csv file for wpp 2024 demographic indicators is not available.
+# FIXME: double check next time because csv files are much easier to deal with
+source_file =  "../source/WPP2024_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT.xlsx"
 
 # %%
-data = pd.read_csv(source_file)
+data = pd.concat([pd.read_excel(source_file, sheet_name="Estimates", skiprows=16),
+                  pd.read_excel(source_file, sheet_name="Medium variant", skiprows=16)], ignore_index=True)
 # %%
 data.head()
 # %%
 for c in data.columns:
     print(c)
+
+data['Year'].unique()  # there are nans in data. I checked and it's because there are some "Label/Separator" lines
+
+
+data = data.dropna(how='any', subset=['Year'])
+
+data
+
+# util functions
+import re
+
+def strip_parentheses(text):
+    return re.sub(r'\s*\([^)]*\)', '', text).strip()
+
+strip_parentheses("Population Sex Ratio, as of 1 July (males per 100 females)")
+
 # %%
 use_indicators = [
     "PopSexRatio",
@@ -33,8 +52,29 @@ use_indicators = [
     "NetMigrations",
     "CNMR"
 ]
+
+use_indicators = [x.lower() for x in use_indicators]
+
+# load metadata
+metadata_file = '../source/metadata.xlsx'
+
+metadata = pd.read_excel(metadata_file, sheet_name='indicators')
+
+# create column name mapping
+colname_mapping = metadata.set_index(['Name'])['concept_id'].to_dict()
+
+colname_mapping
+
+def map_colname(col):
+    return colname_mapping.get(strip_parentheses(col), col)
+
+data.columns.map(map_colname)
+
+data.columns = data.columns.map(map_colname)
+
 # %%
-df = data.set_index(['LocTypeName', 'LocID', 'Time', 'Variant'])[use_indicators].copy()
+# df = data.set_index(['LocTypeName', 'LocID', 'Time', 'Variant'])[use_indicators].copy()
+df = data.set_index(['Type', 'Location code', 'Year', 'Variant'])[use_indicators].copy()
 # %%
 df.head()
 # %%
@@ -46,18 +86,28 @@ df = df.reset_index('Variant', drop=True)
 # %%
 df
 # %%
-gs = df.groupby('LocTypeName')
+# gs = df.groupby('LocTypeName')
+gs = df.groupby('Type')
 # %%
-gs.get_group('World')
+gs.get_group('Special other')
 # %%
-for g, df in gs:
-    df_new = df.reset_index('LocTypeName', drop=True)
-    gid = to_concept_id(g)
+for g, subdf in gs:
+    # df_new = df.reset_index('LocTypeName', drop=True)
+    df_new = subdf.reset_index('Type', drop=True)
+    # rename some type to match metadata
+    if g == 'Special other':
+        gid = "development_group"
+    elif g == 'Region':
+        gid = "geographic_region"
+    else:
+        gid = to_concept_id(g)
     df_new.index.names = [gid, 'time']
 
     for c in df_new.columns:
-        df_serve = df_new[c].dropna()
+        df_serve = df_new[c].replace('...', None).dropna()
         df_serve.to_csv(f'../../demographic_indicators/ddf--datapoints--{c}--by--{gid}--time.csv')
+
+df_new
 # %%
 df.columns.to_frame()
 
